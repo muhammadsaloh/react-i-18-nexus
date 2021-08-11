790a1355a064c70eb2fed9740fd7171f4ad5b505
salohiddin17 <salohiddindilmatov9@gmail.com>
2021-08-11 12:05:57 +0500

    fixed code

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from "react"
-import logo from './logo.svg';
 import './App.css';
 import { useTranslation } from "react-i18next"
 
@@ -9,7 +8,6 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           {t("welcome_msg")}
         </p>
